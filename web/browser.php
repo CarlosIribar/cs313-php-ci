f83b33a442b0c53e64da9d4aa24f91b62c2b95c5
Carlos Inbar <cinbar@cinbar-ltm.internal.salesforce.com>
2018-05-01 16:05:01 -0300

    added jquery

--- web/browser.php
+++ web/browser.php
@@ -5,6 +5,10 @@
 <html>
 <?php require 'products.php';?>
 <body>
+<script
+  src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>
 <h1>Tours</h1>
 <ul>
 
