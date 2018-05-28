bbdd9dc1d1fc879e452b61a25ca76070547991ce
Carlos Iribar <carlosiribar@gmail.com>
2018-05-28 19:57:30 -0300

    print variables

--- web/books.php
+++ web/books.php
@@ -1,30 +1,39 @@
-<?php
-try
-{
-    $dbUrl = getenv('DATABASE_URL');
 
-    $dbopts = parse_url($dbUrl);
-    
-    $dbHost = $dbopts["host"];
-    $dbPort = $dbopts["port"];
-    $dbUser = $dbopts["user"];
-    $dbPassword = $dbopts["pass"];
-    $dbName = ltrim($dbopts["path"],'/');
-    
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
-?>
 <html>
 
 <body>
+    <?php
+    try {
+        // default Heroku Postgres configuration URL
+        $dbUrl = getenv('DATABASE_URL');
 
+        /*if (empty($dbUrl)) {
+        // example localhost configuration URL with postgres username and a database called cs313db
+        $dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
+        }*/
+
+        $dbopts = parse_url($dbUrl);
+        
+        $dbHost = $dbopts["host"];
+        echo $dbHost;
+        $dbPort = $dbopts["port"];
+        echo $dbPort;
+        $dbUser = $dbopts["user"];
+        echo $dbUser;
+        $dbPassword = $dbopts["pass"];
+        echo $dbPassword;
+        $dbName = ltrim($dbopts["path"],'/');
+        echo $dbName;
+        
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex) {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+    }
+    ?>
   <h1>Thanks for your purchase</h1>
   <p> Items </p>
   
