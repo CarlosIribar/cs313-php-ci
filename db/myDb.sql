4b7f262e4f04e1b9effda75a41cbc729570a012d
Carlos Iribar <carlosiribar@gmail.com>
2018-10-08 19:31:36 -0300

    creating db

---
+++ db/myDb.sql
@@ -0,0 +1,25 @@
+
+
+CREATE TABLE Accounts(
+    Id          serial PRIMARY KEY,
+    Name        varchar(120) NOT NULL,
+    Email       varchar(60) NOT NULL,
+    password     varchar(60) NOT NULL
+);
+
+
+CREATE TABLE books(
+    Id          serial PRIMARY KEY,  
+    Name        varchar(120) NOT NULL,
+    Author      varchar(60) NOT NULL,
+    ISBN        varchar(30),
+    UserId      integer REFERENCES Accounts(Id)
+);
+
+CREATE TABLE LectureProgress(
+    Id          serial PRIMARY KEY,
+    StartDate   date NOT NULL,
+    EndDate     date,
+    BookId        integer REFERENCES books(Id),
+    UserId        integer REFERENCES Accounts(Id)
+);
