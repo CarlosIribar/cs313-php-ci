64b18ef230e94abc17d5aed0137041266ad33ca8
Carlos Iribar <carlosiribar@gmail.com>
2018-10-20 11:46:54 -0300

    books page

--- web/books.php
+++ web/books.php
@@ -1,42 +1,70 @@
+<?php
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
 
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>
+<!DOCTYPE html>
 <html>
+<head>
+	<title>Books List</title>
+</head>
 
 <body>
-    <?php
-    try {
-        // default Heroku Postgres configuration URL
-        $dbUrl = getenv('DATABASE_URL');
+<div>
 
-        /*if (empty($dbUrl)) {
-        // example localhost configuration URL with postgres username and a database called cs313db
-        $dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
-        }*/
+<h1>Books List</h1>
+<?php
+$query = "SELECT b.Id, b.Name, b.Author, b.ISBN, a.Name as user FROM books b LEFT JOIN Accounts a ON UserId = a.Id";
+$statement = $db->prepare($query);
+$statement->execute();
 
-        $dbopts = parse_url($dbUrl);
-        
-        $dbHost = $dbopts["host"];
-        echo $dbHost;
-        $dbPort = $dbopts["port"];
-        echo $dbPort;
-        $dbUser = $dbopts["user"];
-        echo $dbUser;
-        $dbPassword = $dbopts["pass"];
-        echo $dbPassword;
-        $dbName = ltrim($dbopts["path"],'/');
-        echo $dbName;
-        
-        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-        
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $ex) {
-        echo 'Error!: ' . $ex->getMessage();
-        die();
-    }
-    ?>
-  <h1>Thanks for your purchase</h1>
-  <p> Items </p>
-  
+echo "<table border='1'>
+<tr>
+<th>Id</th>
+<th>Name</th>
+<th>Author</th>
+<th>ISBN</th>
+<th>User</th>
+</tr>";
+
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{  
+	echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "<td>" . $row['author'] . "</td>";
+    echo "<td>" . $row['isbn'] . "</td>";
+    echo "<td>" . $row['user'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+?>
+
+
+</div>
+
 </body>
+</html>
 
+</div>
+
+</body>
 </html>
