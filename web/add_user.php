ee41f9cd9e2de092393a63d38143e2e4457d5c41
Carlos Iribar <carlosiribar@gmail.com>
2018-11-01 19:56:42 -0300

    feat: logic for add user page

---
+++ web/add_user.php
@@ -0,0 +1,49 @@
+<?php include 'loginDatabase.php';?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Add  User</title>
+</head>
+<style>
+input {
+    height: 20px;
+    width: 30%;
+}
+label {
+    display: block;
+    margin: 5px 0px;
+}
+form {
+    padding: 5px;
+}
+</style>
+
+<body>
+<div>
+<a href="/books.php"> Back to Book List </a>
+<h1>Add User</h1>
+
+<form action="insert_user.php" method="post">
+<label>Name</label>
+<input type="text" placeholder="Name" name="name">
+<br>
+<label>Password</label>
+<input type="password" placeholder="Password" name="password">
+<br>
+<label>Email</label>
+<input type="email" placeholder="email" name="email">
+<br>
+<input type="submit">
+</form>
+
+
+
+</div>
+
+</body>
+</html>
+
+</div>
+
+</body>
+</html>
---
+++ web/insert_user.php
@@ -0,0 +1,32 @@
+<?php include 'loginDatabase.php';?>
+<?php
+
+// get the data from the POST
+$name = $_POST['name'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+
+try
+{
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+	// We do this by preparing the query with placeholder values
+	$query = 'INSERT INTO Accounts(Name, Password, Email)  VALUES(:name, :password, :email)';
+	$statement = $db->prepare($query);
+	// Now we bind the values to the placeholders. This does some nice things
+	// including sanitizing the input with regard to sql commands.
+	$statement->bindValue(':name', $name);
+	$statement->bindValue(':password', $password);
+	$statement->bindValue(':email', $email);
+    $statement->execute();
+}
+catch (Exception $ex)
+{
+	// Please be aware that you don't want to output the Exception message in
+	// a production environment
+	echo "Error with DB. Details: $ex";
+	die();
+}
+header("Location: books.php");
+die(); 
+?>
